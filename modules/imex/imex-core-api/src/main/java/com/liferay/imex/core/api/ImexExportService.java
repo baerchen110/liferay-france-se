8c487235e81936e8b8d2aaf2ffc8de67f1723199
Jean-Paul Da Cunha <dcjeanpaul@gmail.com>
2017-08-28 15:18:44 +0100

    Remove unused files

--- modules/imex/imex-core-api/src/main/java/com/liferay/imex/core/api/ImexExportService.java
+++
@@ -1,7 +0,0 @@
-package com.liferay.imex.core.api;
-
-public interface ImexExportService {
-	
-	public void doExport();
-
-}
--- modules/imex/imex-core-api/src/main/java/com/liferay/imex/core/api/importer/Importer.java
+++
@@ -1,9 +0,0 @@
-package com.liferay.imex.core.api.importer;
-
-import com.liferay.imex.core.api.ImexTask;
-
-public interface Importer extends ImexTask {
-	
-	public void doImport();
-
-}
